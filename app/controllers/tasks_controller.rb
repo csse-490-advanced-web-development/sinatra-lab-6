932a8542e6da91abf90548d3c527ac9047223dcd
Alexander Mohler <mohlerad@rose-hulman.edu>
2024-02-13 14:32:46 -0500

    part 1 done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,35 +13,110 @@ class TasksController < ApplicationController
   end
 
   post '/tasks' do
-    task = Task.new(description: params[:description])
-
-    if task.save
-      redirect "/"
+    if is_json_request?
+	request_body = request.body.read
+	if request_body != ''
+	    json_data = JSON.parse(request_body)
+	    description = json_data['description']
+	else
+	    description = ''
+	end
+	    task = Task.new(user: current_user, description: description)
+	    if task.save
+		status 201
+		json task
+	    else
+		status 400
+		json errors: task.errors.full_messages
+	    end
     else
-      flash.now[:errors] = task.errors.full_messages.join("; ")
-      erb :"tasks/new.html"
+	task = Task.new(user: current_user, description: params[:description])
+	if task.save
+	    redirect "/"
+	else
+	    flash.now[:errors] = task.errors.full_messages.join("; ")
+	    erb :"tasks/new.html"
+	end
     end
   end
 
   get '/tasks/:id' do
-    task = Task.find(params[:id])
-    erb :"tasks/edit.html", locals: { task: task }
+    begin
+	task = Task.find(params[:id])
+	if is_json_request?
+	    if task.user != current_user
+		status 404
+		halt(404, {}.to_json)
+	     else
+		status 200
+	    	json task
+	    end
+	else
+	    erb :"tasks/edit.html", locals: { task: task }
+	end	
+    rescue ActiveRecord::RecordNotFound
+	halt(404, {}.to_json)
+    end
   end
-
+    
   put '/tasks/:id' do
+    begin
     task = Task.find(params[:id])
-    task.description = params[:description]
-    if task.save
-      redirect "/"
+    if task.user != current_user
+	halt(404, {}.to_json)
+    end
+
+    if is_json_request?
+	request_body = request.body.read
+	if request_body != ''
+	    json_data = JSON.parse(request_body)
+	    description = json_data['description']
+	    checked = json_data['complete']
+	else
+	    description = ''
+	    checked = false
+	end
+	task.description = description
+	task.complete = checked
     else
-      flash.now[:errors] = task.errors.full_messages.join("; ")
-      erb :"tasks/edit.html", locals: { task: task }
+	task.description = params[:description]
+    end
+    if task.save
+      if is_json_request?
+	status 200
+	 json task
+      else
+	 redirect "/"
+      end
+    else
+      if is_json_request?
+	 status 400
+	 json errors: task.errors.full_messages
+      else
+	 flash.now[:errors] = task.errors.full_messages.join("; ")
+	 erb :"tasks/edit.html", locals: { task: task }
+      end
+    end
+
+    rescue ActiveRecord::RecordNotFound
+	halt(404, {}.to_json)
     end
   end
 
   delete '/tasks/:id' do
+    begin
     task = Task.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+	halt(404, {}.to_json)
+    end
+    if task.user != current_user
+	    halt(404, {}.to_json)
+    end
     task.destroy!
-    redirect "/"
+    status 204
+    if !is_json_request?
+      redirect "/"
+    end
   end
+
 end
